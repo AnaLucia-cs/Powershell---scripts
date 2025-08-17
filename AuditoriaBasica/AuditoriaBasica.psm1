d3ba5da48112fc1962503b5265545a699cfcda84
Ana Lucia Alonso Martínez <lucia.alonsom@uanl.edu.mx>
2025-08-16 18:26:52 -0600

    Add files via upload

---
+++ AuditoriaBasica/AuditoriaBasica.psm1
@@ -0,0 +1,8 @@
+﻿function Obtener-UsuariosInactivos{
+    Get-LocalUser| Where-Object { $_.Enabled -eq $true -and -not $_.LastLogon }
+}
+ 
+function Obtener-ServiciosExternos {
+    Get-Service | Where-Object { $_.Status -eq "Running" -and $_.DisplayName -notmatch "Windows" }
+}
+ 
---
+++ AuditoriaBasica/mod.ps1
@@ -0,0 +1,6 @@
+﻿New-ModuleManifest -Path '.\AuditoriaBasica.psd1'`
+    -RootModule 'AuditoriaBasica.psm1'`
+    -ModuleVersion '1.0.0'`
+    -Author 'Equio de Ciberseguridad'`
+    -Description 'Modulo para auditoria básica de usuarios y servicios'`
+    -PowerShellVersion '5.1'
